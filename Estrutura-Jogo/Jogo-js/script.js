01045c7e59a0b08fa56d74b6dff6f4db3acb4d45
Victor Stock <victorstock33@gmail.com>
2025-08-03 00:42:15 -0300

    Introduzindo Estrutura

---
+++ Estrutura-Jogo/Jogo-js/script.js
@@ -0,0 +1,6 @@
+            if (document.title === "Jogo do Milhão") {
+                const btnIniciar = document.getElementById("Iniciar");
+                btnIniciar.addEventListener("click", () => {
+                    window.location.href = "/Estrutura-Jogo/Jogo.html";
+                });
+            }
